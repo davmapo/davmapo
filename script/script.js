8cd2cff6faa6c16d7038bd0d982e019d9daa15f9
davmapo <dav.mapelli@gmail.com>
2024-07-22 19:26:47 +0200

    tolto JS risitemato figure

--- script/script.js
+++ script/script.js
@@ -1,2 +1,18 @@
-var queryString = window.location.search;
-console.log(queryString);
+function queryString () {
+// recupero il valore dell'ID dell'URL
+    var qs = window.location.search;
+    return qs;
+}
+
+function title () {
+// costruisco il titolo
+    var qs = queryString ();
+    var htmlfile = "../src/text/" + qs.slice(1, qs.length) + "/" + qs.slice(1, qs.length) + "_testo.html";
+    document.getElementById("#t1") = html "../src/text/1/1_text.html";
+    return htmlfile;    
+}
+
+
+var tl = title();
+console.log(tl);
+
